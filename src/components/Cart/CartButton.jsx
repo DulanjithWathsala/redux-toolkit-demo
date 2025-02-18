25e5ec4e7b2a35b9bef495d4fef89df2a92833f8
M.D.W.De Silva <wdulanjith@gmail.com>
2025-02-18 17:28:30 +0530

    ui slice added to toggle cart

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux";
+
+import { uiActions } from "../../store/ui-slice";
 import classes from "./CartButton.module.css";
 
-const CartButton = (props) => {
+const CartButton = () => {
+  const dispatch = useDispatch();
+
+  const toggleClickHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleClickHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
